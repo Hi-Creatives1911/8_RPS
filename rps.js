5bc37f969b2598463e5ba6704da57002daaf033e
Hi-Creatives1911 <ebereihechukwu@gmail.com>
2024-08-06 08:34:47 +0100

    omo eeeh! my head almost blow...

--- rps.js
+++ rps.js
@@ -1,31 +1,38 @@
-const gameOptions = ["rock", "paper", "scissors"];
-const compOptions = ["bode", "sarah", "buzz"];
+const gameOptions = ["ROCK", "PAPER", "SCISSORS"];
+// const compOptions = ["bode", "sarah", "buzz"];
 function computerOption() {
-   return gameOptions[Math.floor (Math.random() * gameOptions.length)];
+    let compOptions = gameOptions[Math.floor (Math.random() * gameOptions.length)];
+   return compOptions.toUpperCase();
 }
 
 function game (userOption, computerOption) {
-    if (userOption === computerOption) {
+    if (userOption == computerOption) {
         return "It's a TIE!"
     } else if (
-        (userOption === 'rock' && computerOption === 'scissors') ||                
-        (userOption === 'paper' && computerOption === 'rock') ||
-        (userOption === 'scissors' && computerOption === 'paper')) {
+        (userOption == 'rock' && computerOption == 'scissors') ||                
+        (userOption == 'paper' && computerOption == 'rock') ||
+        (userOption == 'scissors' && computerOption == 'paper')) {
         return "You Win!!!"     
-    } else {s
+    } else {
         return "You Lose!!!. Try Again."
     }
 }
 
-let opponent = prompt('Choose your Opponent (Bode, Sarah or Buzz)').toLowerCase();
-let userOption = prompt ('Enter Your Choice [rock, paper or scissors}').toLowerCase();
-if (gameOptions.includes(userOption) && compOptions.includes(opponent)) {
-    let gameComputerOption = computerOption();
-    let result = game(userOption, gameComputerOption);
+// let opponent = prompt('Choose your Opponent (Bode, Sarah or Buzz)');
+// let opponentLower = opponent.toLowerCase();
+// console.log (opponentLower);
+// // while (!compOptions.includes (opponentLower)){
+// //     opponent = prompt('wrong Input. Try again! (Bode, Sarah or Buzz)')
+// // }
 
-    alert ( `\tYou: ${userOption} \n${opponent}: ${gameComputerOption} \n${result}`);
-} else {
-    alert ('Invalid Input. Try Again.')
+let userOption = prompt ('Enter Your Choice [rock, paper or scissors}');
+let userOptionUpper = userOption.toUpperCase();
+while (!(gameOptions.includes (userOptionUpper))){
+    userOption = prompt('wrong Input. Try again! (Rock, Paper or Scissors)')
+    userOptionUpper = userOption.toUpperCase();
 }
 
-    
+let gameComputerOption = computerOption();
+let result = game(userOptionUpper, gameComputerOption);
+
+alert ( `\tYou: ${userOptionUpper} \nComputer: ${gameComputerOption} \n${result}`);
