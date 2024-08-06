16e2e86ae3a37df0a72dc5b2fe3fa57e56d3ea4b
Hi-Creatives1911 <ebereihechukwu@gmail.com>
2024-08-06 13:21:21 +0100

    UPDATED 060824

--- rps.js
+++ rps.js
@@ -1,38 +1,56 @@
 const gameOptions = ["ROCK", "PAPER", "SCISSORS"];
-// const compOptions = ["bode", "sarah", "buzz"];
-function computerOption() {
-    let compOptions = gameOptions[Math.floor (Math.random() * gameOptions.length)];
+
+function computerChoice() {
+    let compOptions = Math.random () < 0.5 ? "rock" : Math.random () < 0.5 ? "paper" : "scissors";
    return compOptions.toUpperCase();
 }
 
-function game (userOption, computerOption) {
-    if (userOption == computerOption) {
-        return "It's a TIE!"
-    } else if (
-        (userOption == 'rock' && computerOption == 'scissors') ||                
-        (userOption == 'paper' && computerOption == 'rock') ||
-        (userOption == 'scissors' && computerOption == 'paper')) {
-        return "You Win!!!"     
-    } else {
-        return "You Lose!!!. Try Again."
+let player = 0;
+let computer = 0;
+let round;
+let result;
+
+function game () {
+    for (let turns = 0; turns < 3; turns++) {
+
+    let userOption = prompt ('Enter Your Choice [rock, paper or scissors}');
+    let userOptionUpper = userOption.toUpperCase();
+
+    while (!(gameOptions.includes (userOptionUpper))){
+        userOption = prompt('wrong Input. Try again! (Rock, Paper or Scissors)')
+        userOptionUpper = userOption.toUpperCase();
     }
-}
 
-// let opponent = prompt('Choose your Opponent (Bode, Sarah or Buzz)');
-// let opponentLower = opponent.toLowerCase();
-// console.log (opponentLower);
-// // while (!compOptions.includes (opponentLower)){
-// //     opponent = prompt('wrong Input. Try again! (Bode, Sarah or Buzz)')
-// // }
+    let computerOption = computerChoice();
 
-let userOption = prompt ('Enter Your Choice [rock, paper or scissors}');
-let userOptionUpper = userOption.toUpperCase();
-while (!(gameOptions.includes (userOptionUpper))){
-    userOption = prompt('wrong Input. Try again! (Rock, Paper or Scissors)')
-    userOptionUpper = userOption.toUpperCase();
-}
+    if (userOptionUpper === computerOption) {
+            alert (`You Chose: ${userOptionUpper}\nComputer Chose: ${computerOption}`)
+            alert (round = "It's a TIE!");
+    } else if (
+            (userOptionUpper === 'ROCK' && computerOption === 'SCISSORS') ||                
+            (userOptionUpper === 'PAPER' && computerOption === 'ROCK') ||
+            (userOptionUpper === 'SCISSORS' && computerOption === 'PAPER')) 
+            {          
+            player++;
+            alert (`You Chose: ${userOptionUpper}\nComputer Chose: ${computerOption}`)
+            alert (round =  `Congrats!\nYou = ${player}\nComputer = ${computer}`);
+            
+    } else {
 
-let gameComputerOption = computerOption();
-let result = game(userOptionUpper, gameComputerOption);
+            computer++;
+            alert (`You Chose: ${userOptionUpper}\nComputer Chose: ${computerOption}`)
+            alert (round =  `Whoops!\nYou = ${player}\nComputer = ${computer}\nTry Again.`);
+        }
 
-alert ( `\tYou: ${userOptionUpper} \nComputer: ${gameComputerOption} \n${result}`);
+    if ((player === 2) || (computer === 2)) {
+            break;
+    }
+} 
+
+if (player === 2) {
+    alert (result = `You Win!!!`);
+} else if (computer === 2) {
+    alert (result = `Computer Wins!!!`);
+} else {alert (result = `UNBELIEVABLE! The Game's a TIE!!!`);}};
+
+game();
